0854545b7a4bc4a3534e672cdcd0b7b773bf9849
rshankars <ravishankar@outlook.in>
2014-07-08 20:25:58 +0530

    WebView demo in Swift
    WebView demo in Swift

---
+++ WebViewDemo/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  WebViewDemo
+//
+//  Created by Ravi Shankar on 08/07/14.
+//  Copyright (c) 2014 Ravi Shankar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController  {
+    
+    @IBOutlet var webView: UIWebView
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let url = NSURL(string: "http://rshankar.com")
+        let request = NSURLRequest(URL: url)
+        webView.loadRequest(request)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func doRefresh(AnyObject) {
+        webView.reload()
+    }
+    
+    @IBAction func goBack(AnyObject) {
+        webView.goBack()
+    }
+    
+    @IBAction func goForward(AnyObject) {
+        webView.goForward()
+    }
+    
+    @IBAction func stop(AnyObject) {
+        webView.stopLoading()
+    }
+
+}
+
